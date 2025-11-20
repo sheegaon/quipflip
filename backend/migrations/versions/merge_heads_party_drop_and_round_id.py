904b6f339efb73d120a20be66dce17d49b7e25f9
Tal Fishman <tfishman@gmail.com>
2025-11-20 11:57:57 -0500

    Add merge migration for party head revisions

---
+++ backend/migrations/versions/merge_heads_party_drop_and_round_id.py
@@ -0,0 +1,19 @@
+"""Merge heads for party removals and round linkage."""
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = '6f3b07d822b9'
+down_revision: Union[str, Sequence[str], None] = (
+    '2b7d3c1fdc4a',
+    'b13a3971c741',
+)
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
