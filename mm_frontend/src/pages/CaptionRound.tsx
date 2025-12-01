c57a3d490af8313c72d8008e6169e92efc4cbf93
Tal Fishman <tfishman@gmail.com>
2025-11-30 21:06:58 -0500

    Remove unused useCallback import from CaptionRound

--- mm_frontend/src/pages/CaptionRound.tsx
+++ mm_frontend/src/pages/CaptionRound.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useGame } from '../contexts/GameContext';
 import { extractErrorMessage } from '../api/client';
